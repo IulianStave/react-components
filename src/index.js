bcd748fc28750c18027ef714a38131c26ee52ec4
iulianstave <iulian.staverescu@gmail.com>
2019-11-27 16:47:10 +0200

    Add two more instance of child component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <CommentDetail/>
+      <CommentDetail/>
       <CommentDetail/>
     </div>
   );
