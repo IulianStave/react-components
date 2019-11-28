48f2d9fba2d0d42c877ae4155534414b4af082b5
iulianstave <iulian.staverescu@gmail.com>
2019-11-28 13:10:15 +0200

    Add more props to render

--- src/index.js
+++ src/index.js
@@ -2,19 +2,36 @@
 // The three tenets of Components
 // Creating a Reusable, Configurable Component
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import faker from 'faker';
+
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam"/>
-      <CommentDetail author="Alina"/>
-      <CommentDetail author="Eve"/>
+      <CommentDetail
+        author="Sam"
+        commentTime="Today at 4:45PM"
+        content="This is a comment"
+        avatarSrc={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="John"
+        commentTime="Today at 2:10PM"
+        content="Another comment"
+        avatarSrc={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Eve"
+        commentTime="Yesterday at 12:15PM"
+        content="Something to say"
+        avatarSrc={faker.image.avatar()}
+      />
     </div>
   );
 };
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
